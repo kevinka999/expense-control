c1f4651459eaddf1db240d672649d2dd1949b8bb
Kevin Katzer <kevinka999@gmail.com>
2025-05-04 18:30:44 -0300

    feat: adding prettier and others package to use later

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router";
 import { router } from "./router";
+import { ExpenseProvider } from "./context/ExpenseContext";
 
 export const App = () => {
   return (
     <main className="p-4 w-full md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto">
-      <RouterProvider router={router} />
+      <ExpenseProvider>
+        <RouterProvider router={router} />
+      </ExpenseProvider>
     </main>
   );
 };
